94eaeb2794463b73ac0af119c5acbba5f5f9a552
mauro-balades <mauro.balades@tutanota.com>
2021-12-27 12:52:38 +0100

    get OS and OS VERSION

--- sf.sh
+++ sf.sh
@@ -25,3 +25,56 @@
 |
 '
 
+# We will start by geting the distro's
+# name and version.
+# OS  = name of distro
+# VER = version
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    # TODO
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    # TODO
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
+# next, we will continue by getting the
+# distribution's icon. by default, we
+# will set it as an UFO.
+# Get it?
+# because it is an unidentified flying object...
+# ASCII arts will need to have a size of 7 rows,
+# 1 space extra to the right and all rows with
+# the same size.
+ICON1="     ___      "
+ICON2=" ___/   \\___ "
+ICON3="/   '---'   \\"
+ICON4="'--_______--' "
+ICON5="     / \\     "
+ICON6="    /   \\    "
+ICON7="   /     \\   "
+
+echo $OS
