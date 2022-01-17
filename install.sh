bb38bf06d1562ce8d7aaa0ffd35cdcb82783c9a8
mauro-balades <mauro.balades@tutanota.com>
2022-01-17 19:23:04 +0100

    added a temporarly branch

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "cloning..."
-git clone https://github.com/mauro-balades/sf.git
+git clone -b code-beauty https://github.com/mauro-balades/sf.git
 
 echo "changin current directory"
 cd sf
@@ -10,7 +10,7 @@ echo "coping shell file to /bin/ (using sudo)..."
 sudo cp sf /bin/sf
 
 echo "coping sf configuration's file..."
-cp config.example.sh "~/.config/sf/config.sh"
+cp "./config.example.sh" "~/.config/sf/config.sh"
 
 echo "cleaning..."
 cd ..
