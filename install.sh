375dc42128bec53be88582c62ac4bb6eae56e15c
mauro-balades <mauro.balades@tutanota.com>
2022-01-17 19:18:40 +0100

    added configuration support

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+echo "cloning..."
 git clone https://github.com/mauro-balades/sf.git
 
+echo "changin current directory"
 cd sf
+
+echo "coping shell file to /bin/ (using sudo)..."
 sudo cp sf /bin/sf
-alias smf="sf"
+
+echo "coping sf configuration's file..."
+cp config.example.sh "~/.config/sf/config.sh"
+
+"cleaning..."
 cd ..
 sudo rm -rf sf
+
+echo "FINISHED!"
